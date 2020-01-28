f88bf16c7fc263b00981d79a9be89d9db0b6e0ad
Duveshka <duveshka@mail.ru>
2020-01-28 16:28:59 +0300

    fix log

--- DatingApp-SPA/src/app/Reminders/Reminders.component.ts
+++ DatingApp-SPA/src/app/Reminders/Reminders.component.ts
@@ -46,7 +46,7 @@ export class RemindersComponent implements OnInit {
         let options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false };
         let timeNow = new Date().toLocaleString('ko-KR', options);
         let timec = new Date(reminder.dateTime).toLocaleString('ko-KR', options);
-        console.log(timeNow);
+        
         if (timeNow == timec) {
             this.pushNotification(reminder.name);
             timec = '';
